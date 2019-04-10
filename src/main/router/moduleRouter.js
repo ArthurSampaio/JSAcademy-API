2222889cc088aced3cfd2fa03572fc1dd1c63336
ArthurSampaio <arthur.sampaio.correia@ccc.ufcg.edu.br>
2019-04-10 17:58:19 -0300

    Add router to /module

---
+++ src/main/router/moduleRouter.js
@@ -0,0 +1,48 @@
+(function () {
+  'use strict';
+
+  var express = require('express');
+  var _ = require('../util/util');
+
+  var UserService = require('../service/userService');
+  var TeamService = require('../service/teamService');
+  var ModuleService = require('../service/moduleService');
+
+  /**
+   * Router used to access the user entity.
+   * URL: /api/user
+   */
+  var moduleRouter = express.Router();
+
+  moduleRouter.get(['', '/'], function (req, res) {
+    return ModuleService.getModules().then(function (response) {
+      return res.status(_.OK).json(response);
+    }).catch(function (error) {
+      return res.status(error.status || _.BAD_REQUEST).json(error.message || error);
+    });
+  });
+
+  moduleRouter.get('/:moduleId', function (req, res) {
+    return ModuleService.getModule(req.params.moduleId).then(function (response) {
+      return res.status(_.OK).json(response);
+    }).catch(function (error) {
+      return res.status(error.status || _.BAD_REQUEST).json(error.message || error);
+    });
+  });
+
+  moduleRouter.post(['', '/'], function (req, res) {
+    return ModuleService.createModule(req.body)
+      .then(function (response) {
+        return res.status(_.CREATED).json(response);
+      })
+      .catch(function (error) {
+        return res.status(error.status || _.BAD_REQUEST).json(error.message || error);
+      });
+  });
+
+
+
+
+
+  module.exports = moduleRouter;
+})();
