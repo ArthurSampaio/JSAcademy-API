a037351c44c92f80db9cbc05702617f53dc59604
ArthurSampaio <arthur.sampaio.correia@ccc.ufcg.edu.br>
2019-04-15 21:31:50 -0300

    Lesson service

---
+++ src/main/service/lessonService.js
@@ -0,0 +1,51 @@
+(function () {
+  'use strict';
+
+  var mongoose = require('mongoose');
+  var _ = require('../util/util');
+  var Lesson = mongoose.model('Lesson');
+
+  /**
+   * Service that handles operations involving exercises.
+   */
+  var LessonService = {};
+
+
+  LessonService.getLessons = function (raw) {
+    var rawLessons = Lesson.find({}).exec();
+
+    return rawLessons.then(function (lesson) {
+      if (!lesson) {
+        throw Error("No exercise founded.");
+      }
+      return raw ?
+        rawLessons : _.map(lesson, function (e) {
+          return e.toObject();
+        })
+    });
+  };
+
+  LessonService.getLesson = function (lessonId, raw) {
+    var params = {
+      _id: lessonId
+    };
+
+    var rawLesson = Lesson.findOne(params).exec();
+    return rawLesson.then(function (exc) {
+      if (!exc) {
+        throw Error("There's no exercise with the given ID: " + lessonId);
+      }
+      return raw ? rawLesson : exc.toObject();
+    });
+  };
+
+  LessonService.createLesson = function (lessonData) {
+
+    return new Lesson(lessonData).save().then(function (saved) {
+      return saved.toObject();
+    });
+  };
+
+
+  module.exports = LessonService;
+})();
