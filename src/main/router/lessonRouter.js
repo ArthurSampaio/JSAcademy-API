dc5da3515e7d24c8dd027280aa5c5f71addb099d
Arthur Sampaio <arthur.sampaio.correia@ccc.ufcg.edu.br>
2019-05-26 17:25:45 -0300

    Add decode for jwt in middleware

--- src/main/router/lessonRouter.js
+++ src/main/router/lessonRouter.js
@@ -1,44 +1,56 @@
-(function () {
-  'use strict';
+;(function() {
+  'use strict'
 
-  var express = require('express');
-  var _ = require('../util/util');
+  var express = require('express')
+  var _ = require('../util/util')
+  var jwt = require('jsonwebtoken')
+  var jwtMiddleware = require('../middleware/jwtMiddleware')
 
-
-  var LessonService = require('../service/lessonService');
+  var LessonService = require('../service/lessonService')
 
   /**
    * Router used to access the Exercise entity.
    * URL: /api/exercise
    */
-  var lessonRouter = express.Router();
+  var lessonRouter = express.Router()
 
-
-  lessonRouter.get(['', '/'], function (req, res) {
-    return LessonService.getLessons().then(function (response) {
-      return res.status(_.OK).json(response);
-    }).catch(function (error) {
-      return res.status(error.status || _.BAD_REQUEST).json(error.message || error);
-    });
-  });
-
-  lessonRouter.get('/:exerciseId', function (req, res) {
-    return LessonService.getLesson(req.params.exerciseId).then(function (response) {
-      return res.status(_.OK).json(response);
-    }).catch(function (error) {
-      return res.status(error.status || _.BAD_REQUEST).json(error.message || error);
-    });
-  });
-
-  lessonRouter.post(['', '/'], function (req, res) {
-    return LessonService.createLesson(req.body)
-      .then(function (response) {
-        return res.status(_.CREATED).json(response);
+  lessonRouter.get(['', '/'], function(req, res) {
+    return LessonService.getLessons()
+      .then(function(response) {
+        return res.status(_.OK).json(response)
       })
-      .catch(function (error) {
-        return res.status(error.status || _.BAD_REQUEST).json(error.message || error);
-      });
-  });
+      .catch(function(error) {
+        return res
+          .status(error.status || _.BAD_REQUEST)
+          .json(error.message || error)
+      })
+  })
 
-  module.exports = lessonRouter;
-})();
+  lessonRouter.get('/:exerciseId', jwtMiddleware, function(req, res) {
+    console.log('>>>>>>>>>>>>>>>>>>>>>a', req.user)
+
+    return LessonService.getLesson(req.params.exerciseId)
+      .then(function(response) {
+        return res.status(_.OK).json(response)
+      })
+      .catch(function(error) {
+        return res
+          .status(error.status || _.BAD_REQUEST)
+          .json(error.message || error)
+      })
+  })
+
+  lessonRouter.post(['', '/'], function(req, res) {
+    return LessonService.createLesson(req.body)
+      .then(function(response) {
+        return res.status(_.CREATED).json(response)
+      })
+      .catch(function(error) {
+        return res
+          .status(error.status || _.BAD_REQUEST)
+          .json(error.message || error)
+      })
+  })
+
+  module.exports = lessonRouter
+})()
