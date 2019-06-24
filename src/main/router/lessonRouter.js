84cfb80153bf65106647ea87f0e15a7823540b13
Arthur Sampaio <arthur.sampaio.correia@ccc.ufcg.edu.br>
2019-06-23 23:10:12 -0300

    Add lesson docs

--- src/main/router/lessonRouter.js
+++ src/main/router/lessonRouter.js
@@ -10,11 +10,17 @@
   var UserService = require('../service/userService')
 
   /**
-   * Router used to access the Exercise entity.
-   * URL: /api/exercise
+   * Router used to access the Lesson and Metrics entities.
+   * URL: /api/lesson
    */
   var lessonRouter = express.Router()
 
+  /**
+   * GET /lesson
+   * Get a list of alls lessons.
+   *
+   * @returns {Promise} Promise with the lessons.
+   */
   lessonRouter.get(['', '/'], jwtMiddleware, function(req, res) {
     const userId = (req.user && req.user._id) || req.user
 
@@ -29,6 +35,11 @@
       })
   })
 
+  /**
+   * GET /lesson/study
+   * Get a list of lessons that the user can answer
+   * @returns {Promise} Promise with the allowed lessons.
+   */
   lessonRouter.get('/study', jwtMiddleware, function(req, res) {
     const userId = (req.user && req.user._id) || req.user
 
@@ -46,21 +57,14 @@
       })
   })
 
-  function getLessons(cb) {
-    return cb
-      .then(function(response) {
-        return res.status(_.OK).json(response)
-      })
-      .catch(function(error) {
-        return error
-          .status(error.status || _.BAD_REQUEST)
-          .json(error.message || error)
-      })
-  }
-
+  /**
+   * GET /lesson/my
+   * Get a list of lessons created by the current user.
+   *
+   * @returns {Promise} A list of lessons.
+   */
   lessonRouter.get('/my', jwtMiddleware, async function(req, res) {
     const userId = (req.user && req.user._id) || req.user
-
     return LessonService.getLessonsByUserId(userId)
       .then(function(response) {
         return res.status(_.OK).json(response)
@@ -72,6 +76,12 @@
       })
   })
 
+  /**
+   * GET /lesson/metrics
+   * Get a list of metrics.
+   *
+   * @returns {Promise} Promise with list of metrics.
+   */
   lessonRouter.get('/metrics', function(req, res) {
     const lessonId = req.query.lessonId
     return LessonService.getMetrics(lessonId)
@@ -85,6 +95,12 @@
       })
   })
 
+  /**
+   * GET /lesson/metrics/metricsId
+   * Get a metric with metricId.
+   *
+   * @returns {Promise} Promise with the metrics.
+   */
   lessonRouter.get('/metrics/:metricsId', jwtMiddleware, async function(
     req,
     res
@@ -100,14 +116,20 @@
       })
   })
 
-  lessonRouter.get('/:exerciseId', jwtMiddleware, async function(req, res) {
+  /**
+   * GET /lesson/lessonId
+   * Get a lesson to answer.
+   *
+   * @returns {Promise} Promise with the  lesson.
+   */
+  lessonRouter.get('/:lessonId', jwtMiddleware, async function(req, res) {
     const userId = (req.user && req.user._id) || req.user
     let anonymous
     if (!userId) {
       anonymous = await UserService.createAnonymousUser()
     }
 
-    return LessonService.getLessonToStudy(req.params.exerciseId, userId)
+    return LessonService.getLessonToStudy(req.params.lessonId, userId)
       .then(function(response) {
         if (anonymous) {
           res.setHeader('anonymous-id', anonymous._id) //works fine
@@ -121,6 +143,12 @@
       })
   })
 
+  /**
+   * POST /lesson/send
+   * Send the answer of a Lesson
+   *
+   * @returns {Promise} Promise with the metrics of lesson.
+   */
   lessonRouter.post('/send', jwtMiddleware, function(req, res) {
     //TODO: logica para pegar se o anonymousId se o req.user for null (nao autenticado)
     const userId = (req.user && req.user._id) || req.user
@@ -140,6 +168,12 @@
       })
   })
 
+  /**
+   * POST /lesson/user
+   * Create a lesson.
+   *
+   * @returns {Promise} Promise with the created lesson.
+   */
   lessonRouter.post(['', '/'], jwtMiddleware, function(req, res) {
     const userId = (req.user && req.user._id) || req.user
 
@@ -154,6 +188,12 @@
       })
   })
 
+  /**
+   * PUT /api/lesson/:lessonId
+   * Update an existing lesson.
+   *
+   * @returns {Promise} Promise with the updated lesson.
+   */
   lessonRouter.put('/:lessonId', function(req, res) {
     return LessonService.update(req.params.lessonId, req.body)
       .then(function(response) {
